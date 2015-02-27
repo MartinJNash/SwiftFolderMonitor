ca3a46cb3e9ee26834a660abc2e10a44458acf9b
Martin Nash <martin.j.nash@gmail.com>
2015-02-27 13:40:55 -0800

    Initial Commit

---
+++ SwiftMonitor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftMonitor
+//
+//  Created by Martin Nash on 2/27/15.
+//  Copyright (c) 2015 Martin Nash. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
