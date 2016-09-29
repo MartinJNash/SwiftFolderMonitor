92eef3536faf110c507f5baec9c2d544fed27383
Ernesto Rivera <rivera.ernesto@gmail.com>
2016-09-29 09:47:08 -0400

    Update to Swift 3

--- FolderMonitor.swift
+++ FolderMonitor.swift
@@ -1,52 +1,47 @@
 
 import Foundation
 
-public class FolderMonitor {
+open class FolderMonitor {
     
     enum State {
-        case On, Off
+        case on, off
     }
     
-    private let source: dispatch_source_t
-    private let descriptor: CInt
-    private let qq: dispatch_queue_t = dispatch_get_main_queue()
-    private var state: State = .Off
+    fileprivate let source: DispatchSource
+    fileprivate let descriptor: CInt
+    fileprivate let qq: DispatchQueue = DispatchQueue.main
+    fileprivate var state: State = .off
     
     /// Creates a folder monitor object with monitoring enabled.
-    public init(url: NSURL, handler: ()->Void) {
+    public init(url: URL, handler: ()->Void) {
 
-        state = .Off
-        descriptor = open(url.fileSystemRepresentation, O_EVTONLY)
+        state = .off
+        descriptor = open((url as NSURL).fileSystemRepresentation, O_EVTONLY)
         
-        source = dispatch_source_create(
-            DISPATCH_SOURCE_TYPE_VNODE,
-            UInt(descriptor),
-            DISPATCH_VNODE_WRITE,
-            qq
-        )
+        source = DispatchSource.makeFileSystemObjectSource(fileDescriptor: descriptor, eventMask: DispatchSource.FileSystemEvent.write, queue: qq) /*Migrator FIXME: Use DispatchSourceFileSystemObject to avoid the cast*/ as! DispatchSource
         
-        dispatch_source_set_event_handler(source, handler)
+        source.setEventHandler(handler: handler)
         start()
     }
     
     /// Starts sending notifications if currently stopped
-    public func start() {
-        if state == .Off {
-            state = .On
-            dispatch_resume(source)
+    open func start() {
+        if state == .off {
+            state = .on
+            source.resume()
         }
     }
     
     /// Stops sending notifications if currently enabled
-    public func stop() {
-        if state == .On {
-            state = .Off
-            dispatch_suspend(source)
+    open func stop() {
+        if state == .on {
+            state = .off
+            source.suspend()
         }
     }
     
     deinit {
         close(descriptor)
-        dispatch_source_cancel(source)
+        source.cancel()
     }
 }
